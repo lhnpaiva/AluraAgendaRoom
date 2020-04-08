5a506abd627988ebf38130228982cf106d3be7ff
lhnpaiva <52859870+lhnpaiva@users.noreply.github.com>
2020-04-08 16:27:02 -0300

    Initial commit

---
+++ app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
@@ -0,0 +1,54 @@
+package br.com.alura.agenda.database;
+
+import android.arch.persistence.db.SupportSQLiteDatabase;
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.migration.Migration;
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import br.com.alura.agenda.database.dao.AlunoDAO;
+import br.com.alura.agenda.model.Aluno;
+
+@Database(entities = {Aluno.class}, version = 3, exportSchema = false)
+public abstract class AgendaDatabase extends RoomDatabase {
+
+    private static final String NOME_BANCO_DE_DADOS = "agenda.db";
+
+    public abstract AlunoDAO getRoomAlunoDAO();
+
+    public static AgendaDatabase getInstance(Context context) {
+        return Room.databaseBuilder(context, AgendaDatabase.class, NOME_BANCO_DE_DADOS)
+                .allowMainThreadQueries()
+                .addMigrations(new Migration(1, 2) {
+                    @Override
+                    public void migrate(@NonNull SupportSQLiteDatabase database) {
+                        database.execSQL("ALTER TABLE aluno ADD COLUMN sobrenome TEXT");
+                    }
+                }, new Migration(2, 3) {
+                    @Override
+                    public void migrate(@NonNull SupportSQLiteDatabase database) {
+                        // Criar nova tabela com as informações desejadas
+                        database.execSQL("CREATE TABLE IF NOT EXISTS `Aluno_novo`" +
+                                " (`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                                "`nome` TEXT, " +
+                                " `sobrenome` TEXT, " +
+                                " `telefone` TEXT, " +
+                                " `email` TEXT)");
+
+                        //Copiar dados da tabela antiga para a nova
+
+                        database.execSQL("INSERT INTO Aluno_novo (id, nome, telefone, email)" +
+                                "SELECT id, nome, telefone, email FROM Aluno");
+
+                        //Remover a tabela antiga
+                        database.execSQL("DROP TABLE Aluno");
+
+                        //Renomear a tabela nova com o nome da tabela antiga
+                        database.execSQL("ALTER TABLE Aluno_novo RENAME TO Aluno");
+                    }
+                })
+                .build();
+    }
+}
